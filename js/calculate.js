40fd076cdebf8d8b41d9b752ed0d7c38faefea92
dev-talukdar <khejurbd@gmail.com>
2023-02-18 23:15:45 +0600

    all staff done

--- js/calculate.js
+++ js/calculate.js
@@ -1,78 +1,100 @@
 // card 2
+
+// add event listener to the add to cart button 
 document.getElementById('cart-rectangle').addEventListener('click', function () {
 
-    // get data from html using ID 
-    const geometryName = document.getElementById('rectangle').innerText; 
-    const geometryValue1 = document.getElementById('rectangle-value1').value; 
+    // get data from html using their ID 
+    const geometryName = document.getElementById('rectangle').innerText;
+    const geometryValue1 = document.getElementById('rectangle-value1').value;
     const geometryValue2 = document.getElementById('rectangle-value2').value;
 
     // check if inputs are valid
     if (isNaN(geometryValue1) || isNaN(geometryValue2) || geometryValue1 <= 0 || geometryValue2 <= 0) {
-       // Error message
+        // Error message, if input invalid data
         alert("Please enter positive numbers for both values");
         return;
     }
 
-    // multiply
-    const totalArea = parseInt(geometryValue1) * parseInt(geometryValue2);     
+    // calculate the total area
+    const totalArea = parseInt(geometryValue1) * parseInt(geometryValue2);
 
-    // display data to the cart section 
+    // display name, total area to the cart section 
     displayData(geometryName, totalArea);
 })
 
-
-
 // card 3 
-document.getElementById('cart-parallelogram').addEventListener('click', function() {
+// add event listener to the calculate button 
+
+document.getElementById('cart-parallelogram').addEventListener('click', function () {
     // Get the values of b and h from the HTML elements
+
     const b = parseInt(document.getElementById('parallelogram-aree3').innerText);
     const h = parseInt(document.getElementById('parallelogram-aree4').innerText);
-  
+
     // Calculate the area of the parallelogram
     const area = b * h;
-  
+
     // Display the result in the table
     displayData('Parallelogram', area.toFixed(2), '1', area.toFixed(2));
-  });
+});
 
 //   card 4
+// add event listener to the calculate button 
+document.getElementById('cart-rhombus').addEventListener('click', function () {
+// Get the values of b and h from the HTML elements
 
-document.getElementById('cart-rhombus').addEventListener('click', function() {
     const d1 = parseInt(document.getElementById('rhombus-area3').innerText);
     const d2 = parseInt(document.getElementById('rhombus-area4').innerText);
+
+    // Calculate the area of the parallelogram
     const area = 0.5 * d1 * d2;
-    displayData('Rhombus', area, '', area, d1, d2);
+
+    // Display the result in the table
+    displayData('Rhombus', area, area, d1, d2);
 });
 
 // card 5 
-document.getElementById('cart-pentagon').addEventListener('click', function() {
+// add event listener to the calculate button 
+document.getElementById('cart-pentagon').addEventListener('click', function () {
+
+    // Get the values of b and h from the HTML elements
     const p = parseFloat(document.getElementById('pentagon-area3').textContent);
     const b = parseFloat(document.getElementById('pentagon-area4').textContent);
+
+    // Calculate the area of the parallelogram
     const area = 0.5 * p * b;
-    displayData('Pentagon',area, p, b);
-  });
+
+    // Display the result in the table
+    displayData('Pentagon', area, p, b);
+});
 
 //   card 6
+// add event listener to the calculate button 
+document.getElementById('cart-ellipse').addEventListener('click', function () {
 
-document.getElementById('cart-ellipse').addEventListener('click', function() {
+    // Get the values of b and h from the HTML elements
     const a = parseFloat(document.getElementById('ellipse-area3').textContent);
     const b = parseFloat(document.getElementById('ellipse-area4').textContent);
+
+    // Calculate the area of the parallelogram
     const area = 3.14 * a * b;
+
+    // Display the result in the table
     displayData('Ellipse', area.toFixed(2), a, b);
-  });
-  
+}); 
 
-
-
-
-function displayData(GeometricName, GeometricPrice) {
+// main function 
+let count = 0;
+function displayData(GeometricName, GeometricArea) {
+    count++;
     const container = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML = `
-        <td>${1}</td>
+        <td>${count}</td>
         <td>${GeometricName}</td>
-        <td>${GeometricPrice}</td> 
+        <td>${GeometricArea} cm<sup>2</sup></td> 
         <td><button class="btn btn-primary bg-cyan-600 border-none hover:bg-blue-500 text-white">Convert to m<sup>2</sup></button></td>
     `;
     container.appendChild(tr);
 }
+
--- js/geometry.js
+++ js/geometry.js
@@ -10,15 +10,15 @@ input1.addEventListener("input", updateOutput);
 input2.addEventListener("input", updateOutput);
 
 // it is used to update output
-function updateOutput() { 
+function updateOutput() {
 
-  // getting values of  input elements
-  const value1 = input1.value;
-  const value2 = input2.value;
+    // getting values of  input elements
+    const value1 = input1.value;
+    const value2 = input2.value;
 
-  //update element with input value
-  output1.textContent = value1;
-  output2.textContent = value2;
+    //update element with input value
+    output1.textContent = value1;
+    output2.textContent = value2;
 }
 
 // getting input elements and cart button
@@ -27,50 +27,49 @@ const value2Input = document.getElementById('triangle-value2');
 const cartButton = document.getElementById('cart-triangle');
 
 // ad event listener to calculate button
-cartButton.addEventListener('click', function() {
-  const value1 = parseFloat(value1Input.value);
-  const value2 = parseFloat(value2Input.value);
-  const total = 0.5 * value1 * value2; 
+cartButton.addEventListener('click', function () {
+    const value1 = parseFloat(value1Input.value);
+    const value2 = parseFloat(value2Input.value);
+    const total = 0.5 * value1 * value2;
 
-   // validate input values
-   if (isNaN(value1) || isNaN(value2) || value1 < 0 || value2 < 0) {
-    // Error message
-    alert("Please enter positive numbers for both values");
-    return;
-  }
+    // validate input values
+    if (isNaN(value1) || isNaN(value2) || value1 < 0 || value2 < 0) {
+        // Error message
+        alert("Please enter positive numbers for both values");
+        return;
+    }
 
-  //area calculation section started
+    //area calculation section started
 
-  const table = document.getElementById('table-container');
-  const rowCount = table.rows.length;
-  const row = table.insertRow(rowCount);
+    const table = document.getElementById('table-container');
+    const rowCount = table.rows.length;
+    const row = table.insertRow(rowCount);
 
-  const noCell = row.insertCell(0);
-  noCell.innerHTML = rowCount + 1;
+    const noCell = row.insertCell(0);
+    noCell.innerHTML = rowCount + 1;
 
-  const nameCell = row.insertCell(1);
-  nameCell.innerHTML = 'Triangle';
+    const nameCell = row.insertCell(1);
+    nameCell.innerHTML = 'Triangle';
 
-  const totalCell = row.insertCell(2);
-  totalCell.innerHTML = total;
+    const totalCell = row.insertCell(2);
+    totalCell.innerHTML = total;
 
-  const actionCell = row.insertCell(3);
-  actionCell.innerHTML = '<button class="btn btn-primary bg-cyan-600 border-none hover:bg-blue-500 text-white">Convert to m<sup>2</sup></button>';
-}); 
+    const actionCell = row.insertCell(3);
+    actionCell.innerHTML = '<button class="btn btn-primary bg-cyan-600 border-none hover:bg-blue-500 text-white">Convert to m<sup>2</sup></button>';
+});
 
 
 // random background color generate
 
 const cards = document.querySelectorAll('.card-random-bg');
 
-  cards.forEach(card => {
+cards.forEach(card => {
     card.addEventListener('mouseover', () => {
-      const randomColor = Math.floor(Math.random()*16777215).toString(16);
-      card.style.backgroundColor = "#" + randomColor;
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        card.style.backgroundColor = "#" + randomColor;
     });
 
     card.addEventListener('mouseleave', () => {
-      card.style.backgroundColor = "white";
+        card.style.backgroundColor = "white";
     });
-  }); 
-  
+});
