68bc1dfc5c11bd2165a3c7a09c60d28037c5ffe1
dev-talukdar <khejurbd@gmail.com>
2023-02-18 09:08:19 +0600

    asset added with private repository

---
+++ requirement.js
@@ -0,0 +1,79 @@
+// // pai er man 3.14 dhorbo 
+
+// 6 ta diffferent dhoreoner shape thakbe, sekhane 2 ta kore input field thakbe 
+// proti ta te calculate button press korle setar man ekta variable e rekhe .. seta abar 
+// dan dik er area calculation box e show korte hobe
+
+// // cm er upper right corner e kivabe 2 dite Hobe eta google korte hobe 
+
+// proti ta card er 2 ta input field e jei man boshabo ami . er pore .. sei man gulo upore .. sei geometric nam er niche.. jei formula deya thakbe.. setar sathe jei 
+// // 2 ta value field thakbe.. oikhane jeye bosheb..ami
+// orthat.. value jeita input dibo.. seta upore jeeye oi  man e boshbe.. ebong calculate kore .. setar Fol  dan dike area calculate section e jeye boshbe. 
+
+// area calculation section e . 3 ta section thakbe 1. geometric name 2 setar result 3. ekta convert to meter button 
+// result er pore 2 ta decimal hote Perbe jemon 10. 15 emon 10. 899873942 emon na 
+
+// 6 ta card er vetore prothom 2 tar validation korte hobe Must baki gula te validation kora important na othoba bonus hisabe 
+
+// validation hobe emon 
+// jodi keu khali rekhe calculate press Kore , tahole waring dibe 
+// jodi keu ekta khali rekhe calculate press Kore , tahole waring dibe 
+// jodi keu ekta string diye  calculate press Kore , tahole waring dibe 
+// jodi keu ekta negative man dise  calculate press Kore , tahole waring dibe 
+
+
+// uporer dan dike jei blog button ta ase sekhane press korle notun ekta page e niye jete Hobe 
+// er pore sekhane 3 ta proshner uttor dite hobe. 
+
+// question 1. what is dom and what's purpose of dom 
+// question 2. kivabe tumi dom theke dom element add korte paro 
+// question 3. event bubble ki jinish
+// question 3. callback function ta ki kaj e lage 
+
+// ****submit korte hobe ***********
+
+// private repository te git push korte Hobe 
+// assignment deploy korte hobe netlify Te 
+
+
+// *************BONUS SECTION ******************
+
+// prothom e ekta kore commit korla
+// erokom kore 8 ta commit korte Hobe 
+// sob gula meaningful commit hote hobe 
+
+// function name id name, variable name, file name 
+// code gula organize hote hobe 
+
+// responsive korte hobe full html document ta 
+// small device er jonno 1 tar niche ekta 
+// er pore last section e jeye 
+// area calculation section ta thakbe.
+
+// area calculation area te jei man gula bosabo.. seigula serial onujai hote hobe 
+// jemon prothom ta press korle 1 
+// pore abar onno ta press korle 2 
+// er pore abar onno ta add korle 3 
+// emon ar ki
+
+
+// 2 ta function ke  kono 2 ta jaigate function re use korte Hobe mane punorai bebohar korte hobe. 
+
+// by default card er color hobe white, jokhon mouse nibo.. tokhon ekta color hobe, abar jokhon ber kore anbo tokhon abar white hobe. er pore jokhon abar nibo .. tokhon abar notun ekta color hobe emon ar ki .
+
+// kivabe random color generate korte Hobe eta google theke generate korte hobe 
+
+// optional 
+
+// area calculation section e jei button ta thakbe.. seta jei button Hobe seta diye jeno convert kora jai 
+// othoba 
+// item jog hobar pore seta jeno abar delete button e press korle remove hoye jabe.. emon ta kore felte paro 
+
+// jokhon edit korbo.. tokhon niche formular niche ekta notun line toiri hobe ar ki 
+// geometric name er pore jei area section ta ase setar pase ekta edit icon diye dibo. 
+// pore abar okay ba click button e click korle.. seta abar input fieeld chole jabe.. eta ke editing toggle type er kichu bole .
+
+
+
+
+
