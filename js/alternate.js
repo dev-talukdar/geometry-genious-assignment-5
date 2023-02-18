c2c6e1db912db9a5a12272cf71f375dc1432e380
dev-talukdar <khejurbd@gmail.com>
2023-02-18 17:06:34 +0600

    blog section added

--- js/alternate.js
+++ js/alternate.js
@@ -1,45 +1,40 @@
-// Triangle calculation  
-
+// Get references to the input and output elements
 const input1 = document.getElementById("triangle-value1");
 const input2 = document.getElementById("triangle-value2");
 const output1 = document.getElementById("triangle-aree1");
 const output2 = document.getElementById("triangle-area2");
 
-// add event listeners to the input elements
+// Add event listeners to the input elements
 input1.addEventListener("input", updateOutput);
 input2.addEventListener("input", updateOutput);
 
-// it is used to update output
-function updateOutput() { 
-
-  // getting values of  input elements
+// Function to update the output elements
+function updateOutput() {
   const value1 = input1.value;
   const value2 = input2.value;
 
-  //update element with input value
+  // Update the output elements with the input values
   output1.textContent = value1;
   output2.textContent = value2;
 }
 
-// getting input elements and cart button
+// Get references to the input elements and cart button
 const value1Input = document.getElementById('triangle-value1');
 const value2Input = document.getElementById('triangle-value2');
 const cartButton = document.getElementById('cart-triangle');
 
-// ad event listener to calculate button
+// Add an event listener to the cart button
 cartButton.addEventListener('click', function() {
   const value1 = parseFloat(value1Input.value);
   const value2 = parseFloat(value2Input.value);
   const total = 0.5 * value1 * value2;
 
-
-//   Rectangle section started
-  //area calculation section started
-
+  // Create new table row to display area calculation
   const table = document.getElementById('table-container');
   const rowCount = table.rows.length;
   const row = table.insertRow(rowCount);
 
+  // Add table cells to the new row
   const noCell = row.insertCell(0);
   noCell.innerHTML = rowCount + 1;
 
